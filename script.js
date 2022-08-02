e62e02f0a58873eb318b3c1c8b82b32f61ed1e0d
viacheslav Kurov <0990004793qw@gmail.com>
2022-08-02 18:19:51 +0300

    added index.html

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+let userName = prompt("Как Вас зовут");
+alert(userName);
+
+let a = 6;
+let b = 3;
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+alert(a ** b);
+
+//или;
+//let c = 6;
+//let d = 3;
+//let addition = a + b;
+//let subtraction = a - b;
+//let multiplication = a * b;
+//let division = a / b;
+//let degree = a ** b;
+//alert(addition);
+//alert(subtraction);
+//alert(multiplication);
+//alert(division);
+//alert(degree);
